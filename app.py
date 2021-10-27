a89db4c75281437b3f2d1715b6e59472041f7bfe
Deepthi <mithudeepu3112@gmail.com>
2021-10-27 09:42:54 +0530

    fixed cyclic import issue

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import dateutil.parser
 from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_migrate import Migrate
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 
 from model import *
 from forms import *
@@ -23,7 +22,7 @@ from forms import *
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object("config")
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
 
 # ----------------------------------------------------------------------------#
